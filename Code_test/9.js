5a3655810872ffc207161b151c65f617ed543d7a
Midas5223 <rksxowns@naver.com>
2022-11-08 22:24:17 +0900

    확인중 제바ㅣㄹ

--- Code_test/9.js
+++ Code_test/9.js
@@ -5,5 +5,5 @@
 var isPalindrome = function (x) {
 	let y = x.toString().split("").reverse().join("");
 	if (x == y) return true;
-	else return false; //내용 변경중//
+	else return false; //내용 변경중//ㅇㅇㅇ
 };
